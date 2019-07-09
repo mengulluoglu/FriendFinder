534d037121870a4d38774c2da337b8a01f6d5957
Idil <idilmengulluoglu@gmail.com>
2019-07-09 19:09:19 -0400

    adding folders

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friends = [{
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Jennifer",
+        photo: "https://en.wikipedia.org/wiki/Jenifer_(singer)#/media/File:Jenifer.png",
+        scores: [
+            2,
+            5,
+            4,
+            1,
+            2,
+            3,
+            3,
+            2,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Mercurio",
+        photo: "https://upload.wikimedia.org/wikipedia/en/3/34/Michael_Mercurio_Actor.jpg",
+        scores: [
+            1,
+            1,
+            5,
+            3,
+            3,
+            5,
+            1,
+            2,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Ben",
+        photo: "https://tr.wikipedia.org/wiki/Ben_Affleck#/media/Dosya:Ben_Affleck_by_Gage_Skidmore_2.jpg",
+        scores: [
+            3,
+            1,
+            3,
+            5,
+            2,
+            3,
+            2,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Jennifer",
+        photo: "https://tr.wikipedia.org/wiki/Jennifer_Lawrence#/media/Dosya:Jennifer_Lawrence_SDCC_2015_X-Men.jpg",
+        scores: [
+            3,
+            1,
+            4,
+            4,
+            2,
+            4,
+            2,
+            5,
+            1,
+            3
+        ]
+    }
+];
+
+module.exports = friends;
